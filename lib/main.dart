31d76ba61c6cf6d93f322ca4a9948252d70548fc
Reahaan Sheriff <reahaansherif@gmail.com>
2021-10-07 20:44:08 +0530

    mobile app chitchat

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:chit_chat/services/auth.dart';
+import 'package:chit_chat/views/home.dart';
+import 'package:chit_chat/views/signin.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: FutureBuilder(
+        future: AuthMethods().getCurrentUser(),
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          } else {
+            return SignIn();
+          }
+        },
+      ),
+    );
+  }
+}
